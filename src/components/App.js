c80147140a544f50391fbf669f02e0fe3e809070
salman3009 <salman444g@gmail.com>
2022-09-25 12:01:13 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -155,7 +155,32 @@ const states = [
 ];
 
 function App() {
-  return <div id="main"></div>;
+  console.log(states);
+  return <div id="main">
+     <ol>
+      {states.map((statesObj,index)=>{
+         return(<div key={`state`+(index+1)}>
+         <li>{statesObj.name}
+             <ol>
+               {statesObj.cities.map((citiesObj,cityindex)=>{
+                    return (<div key={`city`+(cityindex+1)}>
+                    <li>{citiesObj.name}
+                      <ol>
+                        {citiesObj.towns.map((townObj,townIndex)=>{
+                         return (<div key={`town`+(townIndex+1)}>
+                          <li>{townObj.name}</li>
+                         </div>)
+                        })}
+                      </ol>
+                    </li>   
+                    </div>)
+               })}       
+             </ol>
+         </li>
+         </div>)
+      })}
+     </ol>
+  </div>;
 }
 
 export default App;
